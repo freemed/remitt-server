6cfc75f4faddc22596d27d20d370b24a2b2218b2
Jeffrey Buchbinder <jbuchbinder@ravemobilesafety.com>
2025-04-03 09:00:54 -0400

    API: Add version/info endpoint

--- api/version.go
+++ api/version.go
@@ -10,12 +10,21 @@ import (
 func init() {
 	common.ApiMap["version"] = func(r *gin.RouterGroup) {
 		r.GET("/", apiVersion)
+		r.GET("/info", apiInfo)
 		r.GET("/protocol", apiProtocolVersion)
 	}
 }
 
 func apiVersion(c *gin.Context) {
 	c.JSON(http.StatusOK, ProtocolVersion)
+}
+
+func apiInfo(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"version":        ProtocolVersion,
+		"remote_address": c.Request.RemoteAddr,
+		"user":           c.Request.URL.User.Username(),
+	})
 }
 
 func apiProtocolVersion(c *gin.Context) {
